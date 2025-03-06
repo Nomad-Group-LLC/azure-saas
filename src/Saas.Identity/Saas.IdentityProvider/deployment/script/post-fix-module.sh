19c123627d323964611ce6472e617a80b5b920e5
NGDWalker <dwalker@nomadgroup.com>
2025-03-06 14:39:54 -0700

    update for ngt5

--- src/Saas.Identity/Saas.IdentityProvider/deployment/script/post-fix-module.sh
+++ src/Saas.Identity/Saas.IdentityProvider/deployment/script/post-fix-module.sh
@@ -3,7 +3,7 @@
 # Create a random postfix to be used for naming resources.
 function get-postfix() {
     # post_fix="$( LC_CTYPE=C tr -dc 'a-z0-9' < /dev/urandom | fold -w 4 | head -n 1 )"
-    post_fix="ngt3"
+    post_fix="ngt5"
 
     echo "${post_fix}"
     return
